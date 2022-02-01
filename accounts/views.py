017257ecdba709db7f76b84e85175d549d98e975
DESKTOP-D14D69B\amagr <amagramer@gmail.com>
2022-02-02 02:01:35 +0900

    로그아아웃 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.contrib import messages
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, logout_then_login
 from django.shortcuts import redirect, render
 from .forms import SignupForm
 
 login = LoginView.as_view(template_name="accounts/login_form.html")
+
+
+# logout = LogoutView.as_view()
+
+def logout(request):
+    messages.success(request, "로그아웃되었습니다.")
+    return logout_then_login(request)
+
 
 def signup(request):
     if request.method == 'POST':
