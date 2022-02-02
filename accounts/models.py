1945765d5c11c01734dd453ecf1c1a39c03fbce8
DESKTOP-D14D69B\amagr <amagramer@gmail.com>
2022-02-02 21:19:45 +0900

    프로필 수정 ì¶화면 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 
 
 class User(AbstractUser):
+
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "Male"
+        FEMALE = "F", "Female"
+
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
-
+    phone_number = models.CharField(max_length=13, blank=True, validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")])
+    gender = models.CharField(max_length=1, blank=True, choices=GenderChoices.choices, default=GenderChoices.MALE)
 
 # class Profile(models.Model):
 #     pass
