d450afbd9e99049d559e299f871c748cea0f1f31
DESKTOP-D14D69B\amagr <amagramer@gmail.com>
2022-02-02 23:41:39 +0900

    패스워ë드 변경적용

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm as AuthPasswordChangeForm
 
 from accounts.models import User
 
@@ -29,3 +29,12 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'website_url', 'bio', 'phone_number', 'gender']
+
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    def clean_new_password2(self):
+        old_password = self.cleaned_data.get('old_password')
+        new_password2 = super().clean_new_password2()
+        if old_password == new_password2:
+            raise forms.ValidationError("새로운 암호는 기존 암호와 다르게 입력해주세요.")
+        return new_password2
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('password_change/', views.password_change,
+         name='password_change'),
     path('signup/', views.signup, name='signup'),
     path('edit/', views.profile_edit, name='profile_edit'),
 ]
